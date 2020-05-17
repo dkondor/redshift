45d2607880f0553aadce399f7e4587cd93f688f2
Daniel Kondor <kondor.dani@gmail.com>
2020-05-17 23:16:40 +0800

    Interactive settings dialog
    One example way to use the DBus interface
    
    Still missing:
     - option to save settings (should it go into the Python or C code?)
     - interactive setting for location and gamma method or manual times for changes

--- src/redshift-gtk/statusicon_dbus.py
+++ src/redshift-gtk/statusicon_dbus.py
@@ -38,16 +38,34 @@ except ImportError:
 
 from . import defs
 from . import utils
+# ~ import defs
+# ~ import utils
 
 _ = gettext.gettext
 
 
+class _dbus_helper:
+    def __init__(self, name, path, bus = None):
+        if bus is None:
+            bus = Gio.bus_get_sync(Gio.BusType.SESSION, None)
+        self._props = Gio.DBusProxy.new_sync(bus, Gio.DBusProxyFlags.NONE, None,
+                name, path, 'org.freedesktop.DBus.Properties', None)
+        self._name = name
+
+    def get(self, key):
+        return self._props.Get('(ss)', self._name, key)
+
+    def set(self, key, vtype, value):
+        # supported value types: int, double
+        if type(vtype) is not str or len(vtype) != 1:
+            raise ValueError('vtype must be 1 length str!\n')
+        self._props.Set('(ssv)', self._name, key,
+                GLib.Variant(vtype, value))
+
+
 class RedshiftController(GObject.GObject):
     __gsignals__ = {
-        'inhibit-changed': (GObject.SIGNAL_RUN_FIRST, None, (bool,)),
-        'temperature-changed': (GObject.SIGNAL_RUN_FIRST, None, (int,)),
-        'period-changed': (GObject.SIGNAL_RUN_FIRST, None, (str,)),
-        'location-changed': (GObject.SIGNAL_RUN_FIRST, None, (float, float))
+        'value-changed': (GObject.SIGNAL_RUN_FIRST, None, (str,GLib.Variant)),
         }
 
     def __init__(self, name):
@@ -59,10 +77,9 @@ class RedshiftController(GObject.GObject):
                                                 'dk.jonls.redshift.Redshift',
                                                 '/dk/jonls/redshift/Redshift',
                                                 'dk.jonls.redshift.Redshift', None)
-        self._redshift_props = Gio.DBusProxy.new_sync(bus, Gio.DBusProxyFlags.NONE, None,
-                                                      'dk.jonls.redshift.Redshift',
-                                                      '/dk/jonls/redshift/Redshift',
-                                                      'org.freedesktop.DBus.Properties', None)
+        self._redshift_props = _dbus_helper('dk.jonls.redshift.Redshift',
+                                            '/dk/jonls/redshift/Redshift',
+                                            bus)
 
         self._name = name
         self._cookie = None
@@ -71,16 +88,12 @@ class RedshiftController(GObject.GObject):
         self._redshift.connect('g-properties-changed', self._property_change_cb)
 
     def _property_change_cb(self, proxy, props, invalid, data=None):
-        # Cast to python dict as 'in' keyword is broken for GLib dict
-        props = dict(props)
-        if 'Inhibited' in props:
-            self.emit('inhibit-changed', props['Inhibited'])
-        if 'Temperature' in props:
-            self.emit('temperature-changed', props['Temperature'])
-        if 'Period' in props:
-            self.emit('period-changed', props['Period'])
-        if 'CurrentLatitude' in props and 'CurrentLongitude' in props:
-            self.emit('location-changed', props['CurrentLatitude'], props['CurrentLongitude'])
+        n = props.n_children()
+        for i in range(n):
+            x = props.get_child_value(i)
+            key = x.get_child_value(0).get_string()
+            value = x.get_child_value(1).get_variant()
+            self.emit('value-changed', key, value)
 
     @property
     def cookie(self):
@@ -102,27 +115,271 @@ class RedshiftController(GObject.GObject):
 
     @property
     def inhibited(self):
-        return self._redshift_props.Get('(ss)', 'dk.jonls.redshift.Redshift', 'Inhibited')
+        return self._redshift_props.get('Inhibited')
 
     @property
     def temperature(self):
-        return self._redshift_props.Get('(ss)', 'dk.jonls.redshift.Redshift', 'Temperature')
+        return self._redshift_props.get('Temperature')
 
     @property
     def period(self):
-        return self._redshift_props.Get('(ss)', 'dk.jonls.redshift.Redshift', 'Period')
+        return self._redshift_props.get('Period')
 
     @property
     def location(self):
-        lat = self._redshift_props.Get('(ss)', 'dk.jonls.redshift.Redshift', 'CurrentLatitude')
-        lon = self._redshift_props.Get('(ss)', 'dk.jonls.redshift.Redshift', 'CurrentLongitude')
-        return (lat, lon)
+        (lon, lat) = self._redshift_props.get('CurrentLocation')
+        return (lon, lat)
+
+    @property
+    def brightness(self):
+        return self._redshift_props.get('Brightness')
+
+    @brightness.setter
+    def brightness(self,value):
+        if value == 'reset':
+            self._redshift.BrightnessReset()
+        else:
+            self._redshift_props.set('Brightness', 'd', value)
+
+    @property
+    def temperature_day(self):
+        return self._redshift_props.get('TemperatureDay')
+
+    @temperature_day.setter
+    def temperature_day(self, value):
+        self._redshift_props.set('TemperatureDay', 'u', value)
+
+    @property
+    def temperature_night(self):
+        return self._redshift_props.get('TemperatureNight')
+
+    @temperature_night.setter
+    def temperature_night(self, value):
+        self._redshift_props.set('TemperatureNight', 'u', value)
+
+    @property
+    def brightness_day(self):
+        return self._redshift_props.get('BrightnessDay')
+
+    @brightness_day.setter
+    def brightness_day(self, value):
+        self._redshift_props.set('BrightnessDay', 'd', value)
+
+    @property
+    def brightness_night(self):
+        return self._redshift_props.get('BrightnessNight')
+
+    @brightness_night.setter
+    def brightness_night(self, value):
+        self._redshift_props.set('BrightnessNight', 'd', value)
 
     def set_inhibit(self, inhibit):
         if inhibit:
             self._redshift.Inhibit('(i)', self.cookie)
         else:
             self._redshift.Uninhibit('(i)', self.cookie)
+
+    def override_temperature(self, temperature):
+        self._redshift.EnforceTemperature('(iub)', self.cookie,
+            temperature, False)
+
+    def override_temperature_reset(self):
+        self._redshift.UnenforceTemperature('(ib)', self.cookie, False)
+
+
+# group of slider and spinbutton
+class SliderWithSpin(Gtk.Box):
+    __gsignals__ = {
+        'value-changed': (GObject.SIGNAL_RUN_FIRST, None, (int,)),
+        'reset-button': (GObject.SIGNAL_RUN_FIRST, None, ()),
+        }
+    def __init__(self, value = 0, lower = 0, upper = 0, step_incr = 0,
+            have_reset_button = False, label = None):
+        Gtk.Box.__init__(self, orientation = Gtk.Orientation.VERTICAL,
+            spacing = 10)
+
+        # base adjustment
+        self.adj = Gtk.Adjustment(value, lower, upper, step_incr)
+        self.adj.connect('value-changed', self.adj_changed_cb)
+        self._lower = lower
+        self.adj_ignore = lower - 1
+
+        self.scale = Gtk.HScale.new(self.adj)
+        self.scale.set_property('width-request', 200)
+        self.spin = Gtk.SpinButton.new(self.adj, 0.001, 0)
+
+        self.hbox = Gtk.Box(spacing = 10)
+        self.hbox.pack_start(self.scale, True, True, 0)
+        self.hbox.pack_start(self.spin, True, True, 0)
+
+        self.scale.show()
+        self.spin.show()
+
+        if have_reset_button:
+            self.reset_button = Gtk.Button(label=_('Reset'))
+            self.reset_button.connect('clicked', self.reset_cb)
+            self.hbox.pack_start(self.reset_button, True, True, 0)
+            self.reset_button.show()
+        self.hbox.show()
+
+        self.label = Gtk.Label(label)
+        self.label.show()
+        self.pack_start(self.label, True, True, 0)
+        self.pack_start(self.hbox, True, True, 0)
+
+        self.show()
+
+    def set_value(self, value):
+        self.adj_ignore = value
+        self.adj.set_value(value)
+
+    def adj_changed_cb(self, adj):
+        value = adj.get_value()
+        if value != self.adj_ignore:
+            print('value changed: {}'.format(value))
+            self.emit('value-changed', value)
+        self.adj_ignore = self._lower - 1
+
+    def reset_cb(self, resetbutton):
+        self.emit('reset-button')
+
+
+class RedshiftSettingsDialog(Gtk.Dialog):
+    def __init__(self, controller):
+        Gtk.Dialog.__init__(self)
+
+        # Ranges for scale settings
+        self.TEMP_MIN = 2000
+        self.TEMP_MAX = 10000
+        self.TEMP_STEP = 250
+        self.BRIGHTNESS_MIN = 10
+        self.BRIGHTNESS_MAX = 100
+        self.BRIGHTNESS_STEP = 10
+
+        self._controller = controller
+
+        self.set_title(_('Settings'))
+        self.add_button(_('Close'), Gtk.ButtonsType.CLOSE)
+
+        # Day
+        self.day_frame = Gtk.Frame.new('Day')
+        self.day_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL,
+                spacing = 15)
+
+        # temperature slider
+        self.day_temperature = SliderWithSpin(0, self.TEMP_MIN,
+                self.TEMP_MAX, self.TEMP_STEP, False, 'Color temperature')
+        self.day_temperature.set_value(self._controller.temperature_day)
+        self.day_temperature.connect('value-changed', self.day_temperature_adj_cb)
+
+        # brightness slider
+        self.day_brightness = SliderWithSpin(0, self.BRIGHTNESS_MIN,
+                self.BRIGHTNESS_MAX, self.BRIGHTNESS_STEP, False, 'Brightness')
+        self.day_brightness.set_value(round(100*self._controller.brightness_day))
+        self.day_brightness.connect('value-changed', self.day_brightness_adj_cb)
+
+        self.day_box.pack_start(self.day_temperature, True, True, 0)
+        self.day_box.pack_start(self.day_brightness, True, True, 0)
+        self.day_box.show()
+        self.day_frame.add(self.day_box)
+        self.day_frame.show()
+
+        self.get_content_area().pack_start(self.day_frame, True, True, 10)
+
+        # Night
+        self.night_frame = Gtk.Frame.new('Night')
+        self.night_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL,
+                spacing = 15)
+
+        # temperature slider
+        self.night_temperature = SliderWithSpin(0, self.TEMP_MIN,
+                self.TEMP_MAX, self.TEMP_STEP, False, 'Color temperature')
+        self.night_temperature.set_value(self._controller.temperature_night)
+        self.night_temperature.connect('value-changed', self.night_temperature_adj_cb)
+
+        # brightness slider
+        self.night_brightness = SliderWithSpin(0, self.BRIGHTNESS_MIN,
+                self.BRIGHTNESS_MAX, self.BRIGHTNESS_STEP, False, 'Brightness')
+        self.night_brightness.set_value(round(100*self._controller.brightness_night))
+        self.night_brightness.connect('value-changed', self.night_brightness_adj_cb)
+
+        self.night_box.pack_start(self.night_temperature, True, True, 0)
+        self.night_box.pack_start(self.night_brightness, True, True, 0)
+        self.night_box.show()
+        self.night_frame.add(self.night_box)
+        self.night_frame.show()
+
+        self.get_content_area().pack_start(self.night_frame, True, True, 10)
+
+
+        # Override
+        self.override_frame = Gtk.Frame.new('Override')
+        self.override_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL,
+                spacing = 15)
+
+        # temperature slider
+        self.temperature = SliderWithSpin(0, self.TEMP_MIN,
+                self.TEMP_MAX, self.TEMP_STEP, True, 'Color temperature')
+        self.temperature.set_value(self._controller.temperature)
+        self.temperature.connect('value-changed', self.temperature_adj_cb)
+        self.temperature.connect('reset-button', self.temperature_reset_cb)
+
+        # brightness slider
+        self.brightness = SliderWithSpin(0, self.BRIGHTNESS_MIN,
+                self.BRIGHTNESS_MAX, self.BRIGHTNESS_STEP, True, 'Brightness')
+        self.brightness.set_value(round(100*self._controller.brightness))
+        self.brightness.connect('value-changed', self.brightness_adj_cb)
+        self.brightness.connect('reset-button', self.brightness_reset_cb)
+
+        self.override_box.pack_start(self.temperature, True, True, 0)
+        self.override_box.pack_start(self.brightness, True, True, 0)
+        self.override_box.show()
+        self.override_frame.add(self.override_box)
+        self.override_frame.show()
+
+        self.get_content_area().pack_start(self.override_frame, True, True, 10)
+
+        self._controller.connect('value-changed', self.value_change_cb)
+
+
+    def value_change_cb(self, controller, key, value):
+        if key == 'Temperature':
+            self.temperature.set_value(value.get_uint32())
+        elif key == 'Brightness':
+            self.brightness.set_value(round(100*value.get_double()))
+        elif key == 'TemperatureDay':
+            self.day_temperature.set_value(value.get_uint32())
+        elif key == 'BrightnessDay':
+            self.day_brightness.set_value(round(100*value.get_double()))
+        elif key == 'TemperatureNight':
+            self.night_temperature.set_value(value.get_uint32())
+        elif key == 'BrightnessNight':
+            self.night_brightness.set_value(round(100*value.get_double()))
+
+    def brightness_adj_cb(self, slider, value):
+        self._controller.brightness = value / 100.0
+
+    def brightness_reset_cb(self, silder):
+        self._controller.brightness = 'reset'
+
+    def temperature_adj_cb(self, slider, value):
+        self._controller.override_temperature(value)
+
+    def temperature_reset_cb(self, silder):
+        self._controller.override_temperature_reset()
+
+    def day_temperature_adj_cb(self, slider, value):
+        self._controller.temperature_day = value
+
+    def day_brightness_adj_cb(self, slider, value):
+        self._controller.brightness_day = value / 100.0
+
+    def night_temperature_adj_cb(self, slider, value):
+        self._controller.temperature_night = value
+
+    def night_brightness_adj_cb(self, slider, value):
+        self._controller.brightness_night = value / 100.0
+
 
 
 class RedshiftStatusIcon(object):
@@ -179,6 +436,11 @@ class RedshiftStatusIcon(object):
         info_item.connect('activate', self.show_info_cb)
         self.status_menu.append(info_item)
 
+        # Add settings action
+        settings_item = Gtk.MenuItem.new_with_label(_('Settings'))
+        settings_item.connect('activate', self.show_settings_cb)
+        self.status_menu.append(settings_item)
+
         # Add quit action
         quit_item = Gtk.ImageMenuItem.new_with_label(_('Quit'))
         quit_item.connect('activate', self.destroy_cb)
@@ -215,19 +477,30 @@ class RedshiftStatusIcon(object):
         self.info_dialog.get_content_area().pack_start(self.period_label, True, True, 0)
         self.period_label.show()
 
-        self.info_dialog.connect('response', self.response_info_cb)
+        self.brightness_label = Gtk.Label()
+        self.brightness_label.set_alignment(0.0, 0.5)
+        self.brightness_label.set_padding(6, 6)
+        self.info_dialog.get_content_area().pack_start(self.brightness_label, True, True, 0)
+        self.brightness_label.show()
 
-        # Setup signals to property changes
-        self._controller.connect('inhibit-changed', self.inhibit_change_cb)
-        self._controller.connect('period-changed', self.period_change_cb)
-        self._controller.connect('temperature-changed', self.temperature_change_cb)
-        self._controller.connect('location-changed', self.location_change_cb)
+
+        self.info_dialog.connect('response', self.response_info_cb)
+        self.info_dialog.connect('delete-event', self.close_info_cb)
+
+        # Settings dialog
+        self.settings_dialog = RedshiftSettingsDialog(self._controller)
+        self.settings_dialog.connect('response', self.response_settings_cb)
+        self.settings_dialog.connect('delete-event', self.close_settings_cb)
+
+        # Setup signal to property changes
+        self._controller.connect('value-changed', self.change_cb)
 
         # Set info box text
         self.change_inhibited(self._controller.inhibited)
         self.change_period(self._controller.period)
         self.change_temperature(self._controller.temperature)
         self.change_location(self._controller.location)
+        self.change_brightness(self._controller.brightness)
 
         if appindicator:
             self.status_menu.show_all()
@@ -285,6 +558,20 @@ class RedshiftStatusIcon(object):
     def response_info_cb(self, widget, data=None):
         self.info_dialog.hide()
 
+    def close_info_cb(self, widget, data=None):
+        self.info_dialog.hide()
+        return True
+
+    def show_settings_cb(self, widget, data=None):
+        self.settings_dialog.show()
+
+    def response_settings_cb(self, widget, data=None):
+        self.settings_dialog.hide()
+
+    def close_settings_cb(self, widget, data=None):
+        self.settings_dialog.hide()
+        return True
+
     def update_status_icon(self):
         # Update status icon
         if appindicator:
@@ -299,21 +586,23 @@ class RedshiftStatusIcon(object):
                 self.status_icon.set_from_icon_name('redshift-status-off')
 
 
-    # State update functions
-    def inhibit_change_cb(self, controller, inhibit):
-        self.change_inhibited(inhibit)
+    # State update functions -- only one callback to handle all
+    def change_cb(self, controller, key, value):
+        if key == 'Temperature':
+            self.change_temperature(value.get_uint32())
+        elif key == 'Brightness':
+            self.change_brightness(value.get_double())
+        elif key == 'Inhibited':
+            self.change_inhibited(value.get_boolean())
+        elif key == 'Period':
+            self.change_period(value.get_string())
+        elif key == 'CurrentLocation':
+            self.change_location(
+                (value.get_child_value(0).get_double(),
+                value.get_child_value(1).get_double()))
 
-    def period_change_cb(self, controller, period):
-        self.change_period(period)
 
-    def temperature_change_cb(self, controller, temperature):
-        self.change_temperature(temperature)
-
-    def location_change_cb(self, controller, lat, lon):
-        self.change_location((lat, lon))
-
-
-    # Update interface
+    # Update info dialog interface
     def change_inhibited(self, inhibited):
         self.update_status_icon()
         self.toggle_item.set_active(not inhibited)
@@ -326,7 +615,10 @@ class RedshiftStatusIcon(object):
         self.period_label.set_markup(_('<b>Period:</b> {}').format(period))
 
     def change_location(self, location):
-        self.location_label.set_markup(_('<b>Location:</b> {}, {}').format(*location))
+        self.location_label.set_markup(_('<b>Location:</b> {:.3f}, {:.3f}').format(*location))
+
+    def change_brightness(self, brightness):
+        self.brightness_label.set_markup(_('<b>Brightness:</b> {}%').format(round(100*brightness)))
 
 
     def autostart_cb(self, widget, data=None):
